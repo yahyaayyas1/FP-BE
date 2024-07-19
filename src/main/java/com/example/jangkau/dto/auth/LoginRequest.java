171b0920c6aa492b7630eeb85748c367b90c93b8
yahyaayyas1 <yahyaayyas112@gmail.com>
2024-07-19 22:55:20 +0700

    main

--- src/main/java/com/example/jangkau/dto/auth/LoginRequest.java
+++ src/main/java/com/example/jangkau/dto/auth/LoginRequest.java
@@ -1,7 +1,5 @@
 package com.example.jangkau.dto.auth;
 
-import com.example.jangkau.services.ValidPassword;
-import com.example.jangkau.services.ValidUsername;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
@@ -9,9 +7,7 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class LoginRequest {
     @NotEmpty(message = "must not empty")
-    @ValidUsername
     private String username;
     @NotEmpty(message = "must not empty")
-//    @ValidPassword
     private String password;
 }
