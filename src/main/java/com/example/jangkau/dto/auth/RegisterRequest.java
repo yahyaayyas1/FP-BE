1b80a083e3092df6c92dd60d55c7aa5deb3be2e4
yahyaayyas1 <yahyaayyas112@gmail.com>
2024-07-20 17:06:32 +0700

    add

--- src/main/java/com/example/jangkau/dto/auth/RegisterRequest.java
+++ src/main/java/com/example/jangkau/dto/auth/RegisterRequest.java
@@ -1,9 +1,10 @@
 package com.example.jangkau.dto.auth;
 
+import com.example.jangkau.services.ValidPassword;
+import com.example.jangkau.services.ValidUsername;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import com.example.jangkau.services.ValidUsername;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -20,6 +21,7 @@ public class RegisterRequest {
     @Email
     private String emailAddress;
     @NotEmpty(message = "must not empty")
+    @ValidPassword
     private String password;
     @NotBlank(message = "Must not empty")
     private String fullName;
